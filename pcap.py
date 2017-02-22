8f12a0bdfb8dddb77718d4f786203100a21b4c8a
rmaratos <rmaratos@andrew.cmu.edu>
2017-02-22 12:27:42 -0500

    Initial commit with 18859 code

---
+++ pcap.py
@@ -0,0 +1,146 @@
+from scapy.all import rdpcap
+import logging, math, subprocess
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+from Tkinter import *
+
+SENT = 1
+RECEIVED = 2
+BUCKET_SIZE = 0.1
+def get_ds(packet):
+    return packet.FCfield & 0x3
+
+
+class Packet(object):
+    """ Extract useful info from Scapy Packet"""
+    def __init__(self, packet):
+        #self.packet = packet
+        self.time = packet.time
+        self.ds = get_ds(packet)
+        try:
+            self.size = len(packet.wepdata)
+        except:
+            self.size = 0
+
+class Client(object):
+    def __init__(self, packet, mac):
+        self.mac = mac
+        self.sent_packets = []
+        self.received_packets = []
+        self.packets = []
+        self.add_packet(packet)
+
+    def add_packet(self, packet):
+        # print "add packet", self.mac, ds
+        self.packets.append(packet)
+        if packet.ds == SENT:
+            self.sent_packets.append(packet)
+        elif packet.ds == RECEIVED:
+            self.received_packets.append(packet)
+        else:
+            client_mac = None
+            print "client_mac None"
+
+    def bandwidth(self):
+        sent = sum(map(len, self.sent_packets))
+        received = sum(map(len, self.received_packets))
+        print "sent\t\t%d\nreceived\t%d" % (sent, received)
+
+    def timing(self):
+        t = None
+        for p in self.packets:
+            if t == None:
+                print 0
+                t = p.time
+                continue
+            print 1000*(p.time - t)
+            t = p.time
+
+    def buckets(self):
+        start = self.packets[0].time
+        end = self.packets[-1].time
+        delta = math.ceil(end - start)
+        num_buckets = int(math.ceil(delta / BUCKET_SIZE))
+        self.sent_buckets = [0] * num_buckets
+        self.received_buckets = [0] * num_buckets
+        for packet in self.sent_packets:
+            t = packet.time
+            i = int((t - start) / BUCKET_SIZE)
+            self.sent_buckets[i] += packet.size
+        for packet in self.received_packets:
+            t = packet.time
+            i = int((t - start) / BUCKET_SIZE)
+            self.received_buckets[i] += packet.size
+        # print self.sent_buckets
+        # print self.received_buckets
+        return BUCKET_SIZE, delta
+
+
+    def __repr__(self):
+        return self.mac + ' (%d,%d)' % (len(self.sent_packets), len(self.received_packets))
+
+
+class Monitor(object):
+    def __init__(self, pcap, mac_addr):
+        self.pcap = pcap
+        self.mac_addr = mac_addr
+        #self.clients = {}
+        self.client = None
+        self.scan_pcap()
+
+    def scan_pcap(self):
+        print "scan_pcap START"
+        cmd = "sudo tcpdump -nnvs0 -I -i en0 -G 5 -W 1 -w %s ether host %s" % (self.pcap, self.mac_addr)
+        print "calling command", cmd
+        subprocess.call(cmd, shell=True)
+        print "command done, reading pcap"
+        packets = rdpcap(self.pcap)
+        # maybe ignore retry packets too?
+        # data_packets = filter(lambda p:(p.type==2 and p.subtype==8), packets)
+        #data_packets = []
+        # count = 0
+        print "scan_pcap READ"
+        for p in packets:
+            # count += 1
+            try:
+                if (p.type==2 and p.subtype==8):
+                    #60:03:08:93:9c:54
+                    if (p.addr2==self.mac_addr or p.addr1==self.mac_addr):
+                        #data_packets.append(p)
+                        packet = Packet(p)
+
+                        if packet.ds == SENT:
+                            client_mac = p.addr2
+                        elif packet.ds == RECEIVED:
+                            client_mac = p.addr1
+                        else:
+                            client_mac = None
+                            # print "client_mac None"
+                        # print ds, packet.addr1, packet.addr2, packet.addr3, packet.addr4
+                        #if client_mac in self.clients:
+                        if self.client == None:
+                             print "!!!setting client"
+                             self.client = Client(packet, client_mac)
+                        else:
+                            self.client.add_packet(packet)
+            except:
+                pass
+        print "scan_pcap DONE"
+
+                # p.show()
+            # if (count % 100) == 0:
+            #     print count
+
+        #for raw_packet in data_packets:
+        #print self.clients
+
+# m = Monitor('80211.pcap')
+# c = m.clients['78:31:c1:c5:8f:4e']
+#
+# total_bytes = 0
+# # for p in c.received_packets:
+# #     print p.time, len(p.wepdata)
+# #     total_bytes += len(p.wepdata)
+# #
+# # print total_bytes / (c.packets[-1].time - c.packets[0].time)
+#
+# c.buckets()
---
+++ visual.py
@@ -0,0 +1,209 @@
+from tkinter import Tk, Canvas, CENTER, ALL, Entry, TOP, BOTH
+import random
+from enum import Enum
+import matplotlib
+matplotlib.use('TkAgg')
+
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import numpy as np
+from pcap import Monitor
+
+import sys
+
+class Mode(Enum):
+    """Animation Modes"""
+    START = 0
+    SETUP = 1
+    MAIN = 2
+
+
+class Button(object):
+    """Basic Button Class for Animations"""
+    def __init__(self, cx, cy, width, height, text):
+        self.cx, self.cy = cx, cy
+        self.x0 = cx - width/2
+        self.x1 = cx + width/2
+        self.y0 = cy - height/2
+        self.y1 = cy + height/2
+        self.text = text
+
+    def clicked(self, x, y):
+        """Click on rectangular region of button"""
+        return (self.x0 <= x <= self.x1) and (self.y0 <= y <= self.y1)
+
+    def draw(self, canvas):
+        """Draw button rectangle and text"""
+        cx, cy = self.cx, self.cy
+        canvas.create_text(cx, cy, text=self.text, font=("Impact", 32))
+        canvas.create_rectangle(self.x0, self.y0, self.x1, self.y1)
+
+class Visual(object):
+    def __init__(self, pcap_file, mac, width=800, height=600):
+        root = Tk()
+        self.root = root
+        self.margin = 0.12*height
+        self.width, self.height = width, height - self.margin
+        self.cx, self.cy = width/2, (height - self.margin)/2
+        self.toolbar = \
+            Canvas(self.root, width=self.width, height=self.margin)
+        self.toolbar.pack()
+
+        self.pcap_file = pcap_file
+        self.mac = mac
+        self.canvas = Canvas(root, width=width, height=height - self.margin)
+        # self.canvas.pack()
+        self.init_animation()
+        root.bind("<Button-1>", lambda e: self.mouse_event(e))
+        root.bind("<Key>", lambda e: self.key_event(e))
+        root.mainloop()
+
+
+    def draw_start_screen(self):
+        """Start screen text"""
+        self.canvas.delete(ALL)
+        cx, cy = self.width/2, self.height/2
+        font = ("Impact", "128")
+        self.canvas.create_text(cx, 0.8*cy, text="Wi-Fi Snooper", font=font)
+        font = ("Impact", 32)
+        self.canvas.create_text(cx, 1.5*cy, text="Press s to Begin", font=font)
+
+    def draw_toolbar(self):
+        """Toolbar for main animation"""
+        self.toolbar.create_text(self.cx, 0.1*self.cy, text=sys.argv[1],
+                                 font=("Impact", 32))
+        self.toolbar.create_text(self.cx, 0.3*self.cy, text="Type of Activity: ",
+                                 font=("Impact", 32))
+        self.toolbar.create_text(self.cx*0.05, 0.1*self.cy,
+                                 text="Q to quit",
+                                 font=("Impact", 20), anchor="w")
+
+    def draw_all(self):
+        """Refresh frame for infection animation"""
+        print "redraw_all"
+        # self.canvas.delete(ALL)
+        f = Figure(figsize=(5, 4), dpi=100)
+        self.sent_subplot = f.add_subplot(121)
+        self.sent_subplot.set_xlabel('Time (seconds)')
+        self.sent_subplot.set_ylabel('Bandwidth (bytes)')
+        self.sent_subplot.set_title('Sent Traffic')
+        self.received_subplot = f.add_subplot(122)
+        self.received_subplot.set_xlabel('Time (seconds)')
+        self.received_subplot.set_ylabel('Bandwidth (bytes)')
+        self.received_subplot.set_title('Received Traffic')
+
+
+
+        # self.canvas.destroy()
+        self.canvas2 = FigureCanvasTkAgg(f, master=self.root)
+        self.canvas2.show()
+        self.canvas2.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
+
+
+
+        #
+        # #running FFT on the data
+        #
+        # #t = np.arange(256)
+        # sp = np.fft.fft(c.received_buckets)
+        # freq = np.fft.fftfreq(t.shape[-1])
+        # plt.plot(freq, sp.real, freq, sp.imag)
+        # plt.show()
+
+
+
+        self.update_plot()
+        print "raised"
+
+    def update_plot(self):
+        m = Monitor(self.pcap_file, self.mac)
+        c = m.client #computer
+        if not c:
+            print "C is none"
+            return
+        bucket, delta = c.buckets()
+        print "SENT"
+        print c.sent_buckets
+        print "RECEIVED"
+        print c.received_buckets
+        t = np.arange(0, delta, bucket)
+        self.sent_subplot.clear()
+        self.received_subplot.clear()
+        self.sent_subplot.axis([0,delta,0, max(c.sent_buckets)])
+        self.sent_subplot.plot(t, c.sent_buckets)
+        self.sent_subplot.axis([0,delta,0, max(c.received_buckets)])
+        self.received_subplot.plot(t, c.received_buckets)
+        # plt.pause(0.05)
+        self.canvas2.draw()
+
+
+    def timer_fired(self):
+        """Called every frame refresh"""
+        print "timer_fired", self.mode
+        if self.mode == Mode.START:
+            pass
+        if self.mode == Mode.MAIN and not self.paused:
+            self.update_plot()
+
+    def timer(self):
+        """Setup timer loop"""
+        self.timer_fired()
+        self.canvas.after(self.timer_delay, self.timer)
+
+    def init_animation(self):
+        """Initialize or reset animation"""
+        self.users = []
+        self.timer_delay = 10
+        self.start_counter = 0
+        self.versions = 40
+        self.max_users = 100
+        self.version = None
+        self.paused = False
+        # self.mode = Mode.START
+        # self.draw_start_screen()
+        self.mode = Mode.MAIN
+        # self.redraw_all()
+        self.error_text = None
+        self.error_font_size = 20
+        self.version_select = None
+        self.start()
+        self.timer()
+
+    def start(self):
+        """Initialize and start"""
+        self.draw_toolbar()
+        self.mode = Mode.MAIN
+        self.draw_all()
+
+
+    def mouse_event(self, e):
+        """Process click event"""
+        x, y = e.x, e.y
+        if self.mode == Mode.SETUP:
+            if self.button.clicked(x, y):
+                self.start()
+        if self.mode == Mode.MAIN:
+            pass
+
+    def key_event(self, e):
+        """Process keyboard event"""
+        if e.keysym == 'r':
+            self.init_animation()
+            self.paused = False
+        elif e.keysym == 'p':
+            self.paused = not self.paused
+        elif e.keysym == 's' and self.mode == Mode.START:
+            self.mode = Mode.MAIN
+            # self.draw_setup_screen()
+            self.start()
+        elif e.keysym == 'b' and self.mode == Mode.SETUP:
+            self.start()
+        elif e.keysym == 'q':
+            self.root.destroy()
+
+if len(sys.argv) != 3:
+    print "Usage visual.py pcap_file mac_address"
+else:
+    Visual(sys.argv[1], sys.argv[2])
